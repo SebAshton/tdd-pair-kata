db99fb903f700246aa4afa19fb7e5859227d7b9a
Seb Ashton <seb@sebashton.com>
2017-09-15 16:07:00 +0100

    Big bang commit

--- main.py
+++ main.py
@@ -1,6 +1,43 @@
-def inc(x):
-    return x + 1
+class Gate(object):
+    def __init__(self, current_state="locked"):
+        self.__state = current_state
+        self.__actions = {}
 
+        self.listen(state="locked", event="coin", action=lambda gate: gate.go_to('unlocked'))
+        self.listen(state="locked", event="through", action=lambda gate: gate.alarm())
+        self.listen(state="locked", event="power", action=lambda gate: gate.go_to('no_entry'))
+        self.listen(state="no_entry", event="power", action=lambda gate: gate.go_to('locked'))
+        self.listen(state="no_entry", event="through", action=lambda gate: gate.alarm())
+        self.listen(state="unlocked", event="through", action=lambda gate: gate.go_to('locked'))
+        self.listen(state="unlocked", event="coin", action=lambda gate: gate.thank_you())
+        self.listen(state="unlocked", event="power", action=lambda gate: gate.go_to('no_entry'))
 
-def test_in_a_module():
-    assert inc(7) == 9
+    def alarm(self):
+        print "ALARM ALARM"
+
+    def thank_you(self):
+        print "THANK YOU <3"
+
+    def listen(self, state, event, action):
+        key = "{0}:{1}".format(state, event)
+        self.__actions[key] = self.__actions.get(key) or []
+        self.__actions[key].append(action)
+
+    def current_state(self):
+        return self.__state
+
+    def go_to(self, state):
+        self.__state = state
+
+    def coin(self): self.__trigger("coin")
+    def through(self): self.__trigger("through")
+    def power(self): self.__trigger("power")
+
+    def __trigger(self, event):
+        key = "{0}:{1}".format(self.current_state(), event)
+        actions = self.__actions.get(key) or []
+        [action(self) for action in actions]
+
+    def __events_for_state(self):
+        return self.__transitions[self.current_state()]
+
--- main_test.py
+++ main_test.py
@@ -1,5 +1,69 @@
-import main
+from main import Gate
+import os
 
+def test_can_create_a_gate():
+    gate = Gate()
+    assert gate.current_state() == "locked"
 
-def test_answer():
-    assert main.inc(3) == 5
+def test_can_unlock_with_a_coin():
+    gate = Gate()
+    gate.coin()
+
+    assert gate.current_state() == "unlocked"
+
+def test_can_unlock_and_pass_through_a_gate():
+    gate = Gate()
+    gate.coin()
+    gate.through()
+
+    assert gate.current_state() == "locked"
+
+def test_adding_listener_for_an_action(mocker):
+    stub = mocker.stub(name='on_locked_coin')
+    gate = Gate()
+    gate.listen(state="locked", event="coin", action=stub)
+    gate.coin()
+
+    stub.assert_called_once_with(gate)
+
+def test_will_alarm_when_passing_through_a_locked_gate(mocker):
+    gate = Gate()
+    mocker.spy(gate, 'alarm')
+
+    gate.through()
+
+    assert gate.alarm.call_count == 1
+
+def test_will_thank_you_when_pay_twice(mocker):
+    gate = Gate(current_state="unlocked")
+    mocker.spy(gate, 'thank_you')
+
+    gate.coin()
+
+    assert gate.thank_you.call_count == 1
+
+def test_will_forbid_entry_when_unlocked_and_powering_off():
+    gate = Gate(current_state="unlocked")
+    gate.power()
+
+    assert gate.current_state() == "no_entry"
+
+def test_will_forbid_entry_when_locked_and_powering_off():
+    gate = Gate()
+    gate.power()
+
+    assert gate.current_state() == "no_entry"
+
+def test_will_lock_when_powering_on():
+    gate = Gate(current_state="no_entry")
+    gate.power()
+
+    assert gate.current_state() == "locked"
+
+def test_will_alarm_when_entering_and_powered_off(mocker):
+    gate = Gate(current_state="no_entry")
+    mocker.spy(gate, 'alarm')
+
+    gate.through()
+
+    assert gate.alarm.call_count == 1
